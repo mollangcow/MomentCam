925a9214d2e0aaee1bb612f64e710e8cecc0b316
SangwooKim <mollangcow@gmail.com>
2023-08-30 16:29:39 +0900

    create project

---
+++ MomentCam/MomentCam/MomentCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentCamApp.swift
+//  MomentCam
+//
+//  Created by kimsangwoo on 2023/08/30.
+//
+
+import SwiftUI
+
+@main
+struct MomentCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ MomentCam/MomentCam/View/MainView.swift
@@ -0,0 +1,26 @@
+//
+//  MainView.swift
+//  MomentCam
+//
+//  Created by kimsangwoo on 2023/08/30.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
